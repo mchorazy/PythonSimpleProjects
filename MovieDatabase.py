741910aebe0f1c2df30a89be4c3152227b2d1e97
mchorazy <mchorazy91@gmail.com>
2017-09-21 10:41:21 +0200

    Initial Project 2

---
+++ MovieDatabase.py
@@ -0,0 +1,23 @@
+class databaseMovie:
+    def __init__(self):
+        self.databaseMovie = {}
+
+    def addMovie(self, movie, details=[]):
+        self._movie = movie
+        self._details = details
+        self.databaseMovie[self._movie] = self._details
+
+    def printDatabase(self):
+        return self.databaseMovie
+
+    def deleteMovie(self, movie):
+        if movie in self.databaseMovie:
+            del self.databaseMovie[movie]
+        else:
+            print("Movie not found!")
+
+if __name__ == "__main__":
+    movieDB = databaseMovie()
+    movieDB.addMovie('Gladiator', [1980,'Ridley','Drama'])
+    movieDB.addMovie('Batman', [1999, 'john', 'Scif-fi'])
+    print(movieDB.printDatabase())
